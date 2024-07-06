c6a8fbb5427797538272ad8bfc1c7d3bb3acd4d1
reemr <reemramdan66@gmail.com>
2024-07-06 18:03:07 +0300

    adding testng  tcases  commit

--- src/test/java/t/T.java
+++ src/test/java/t/T.java
@@ -3,20 +3,55 @@ package t;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
 
 public class T {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+
+    WebDriver driver = new ChromeDriver();
+
+    @Test
+    public void tCase1(){
+            driver.get("https://the-internet.herokuapp.com/");
+            driver.findElement(By.linkText("Form Authentication")).click();
+            driver.findElement(By.name("username")).sendKeys("tomsmith");
+            driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+            driver.findElement(By.className("radius")).click();
+            String actualResult = driver.findElement(By.id("flash")).getText();
+            String expectedResult = "You logged into a secure area!";
+            assertEquals(actualResult,expectedResult);
+            assertTrue(actualResult.contains(expectedResult));
+
+    }
+/*
+    public void tCase2(){
         driver.get("https://the-internet.herokuapp.com/");
         driver.findElement(By.linkText("Form Authentication")).click();
         driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword23!");
+        driver.findElement(By.className("radius")).click();
+        String actualResult = driver.findElement(By.id("flash")).getText();
+        String expectedResult = "You logged into a secure area!";
+        assertEquals(actualResult,expectedResult);
+        assertTrue(actualResult.contains(expectedResult));
+
+    }
+
+    public void tCase3(){
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.name("username")).sendKeys("tomsmith23");
         driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
         driver.findElement(By.className("radius")).click();
         String actualResult = driver.findElement(By.id("flash")).getText();
         String expectedResult = "You logged into a secure area!";
         assertEquals(actualResult,expectedResult);
+        assertTrue(actualResult.contains(expectedResult));
 
     }
+
+ */
 }
