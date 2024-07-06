05865c54598912b6afb763ff39a0574af0a0b319
reemr <reemramdan66@gmail.com>
2024-07-06 14:57:03 +0300

    adding main method with test cases  commit

--- src/test/java/t/T.java
+++ src/test/java/t/T.java
@@ -1,11 +1,22 @@
 package t;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.testng.Assert.assertEquals;
 
 public class T {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/login");
+        driver.get("https://the-internet.herokuapp.com/");
+        driver.findElement(By.linkText("Form Authentication")).click();
+        driver.findElement(By.name("username")).sendKeys("tomsmith");
+        driver.findElement(By.name("password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.className("radius")).click();
+        String actualResult = driver.findElement(By.id("flash")).getText();
+        String expectedResult = "You logged into a secure area!";
+        assertEquals(actualResult,expectedResult);
+
     }
 }
